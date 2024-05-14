3d27c1061f141572b7cfc2602c1d6548a4e12323
vanshchugh9079 <vanshchugh67@gmail.com>
2024-05-14 08:47:14 +0530

    send data to db

--- index.js
+++ index.js
@@ -4,25 +4,27 @@ const mongoose = require('mongoose');
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
+const {PORT,USER_NAME,PASSWORD} =process.env
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
 async function main() {
-    return await mongoose.connect('mongodb+srv://vanshchugh9079:a1s1d1f1g1@cluster15.h7tlojn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster15', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    
+    try {
+        let dbresponse=await mongoose.connect(`mongodb+srv://${USER_NAME}:${PASSWORD}@cluster15.h7tlojn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster15`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        let Student=mongoose.model("student",{name:String,age:Number,class:Number})
+        let vansh=new Student({name:"vansh",age:20,class:12})
+        vansh.save().then(data=>console.log(data))
+    } catch (error) {
+        console.log(error);
+    }
+  
 }
-
 main()
-    .then((data) => {
-        console.log(data.mquery());
-        app.get('/', (req, res) => {
-            res.send("Hello World");
-        });
 
-        app.listen(PORT, () => {
-            console.log(`Server listening on port ${PORT}`);
-        });
-    })
-    .catch(err => console.error(err));
